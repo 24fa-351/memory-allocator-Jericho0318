6353b8686191ad07d53f8727251404a915398ef7
Jericho0318 <111917174+Jericho0318@users.noreply.github.com>
2024-12-10 20:44:16 -0800

    Update system_malloc.c

--- system_malloc.c
+++ system_malloc.c
@@ -6,26 +6,28 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        perror("Provide 3 arguments: ./<filename>, <size to malloc>, <size to realloc>\n");
+        perror(
+            "Provide 3 arguments: ./<filename>, <size to malloc>, <size to "
+            "realloc>\n");
         exit(1);
     }
     int size = atoi(argv[1]);
     int* ptr;
     printf("Allocated memory:\n");
-    for(int ix = 0; ix < MALLOC_NUM_OF_ITER; ++ix) {
+    for (int ix = 0; ix < MALLOC_NUM_OF_ITER; ++ix) {
         ptr = malloc(size);
-        if(ptr == NULL) {
+        if (ptr == NULL) {
             fprintf(stderr, "Failed to allocate memory.\n");
             exit(1);
         }
         printf("%p,\n", ptr);
-    } 
+    }
 
     int realloc_size = atoi(argv[2]);
     printf("Reallocated memory:\n");
-    for(int ix = 0; ix < REALLOC_NUM_OF_ITER; ++ix) {
+    for (int ix = 0; ix < REALLOC_NUM_OF_ITER; ++ix) {
         ptr = realloc(ptr, size);
-        if(ptr == NULL) {
+        if (ptr == NULL) {
             fprintf(stderr, "Failed to allocate memory.\n");
             exit(1);
         }
